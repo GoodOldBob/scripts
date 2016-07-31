7acec1d011646dc5e891066533ff363661ce8b8a
GoodOldBob <bobfan03@gmail.com>
2016-07-31 14:32:55 -0700

    importability

--- simple_rss.py
+++ simple_rss.py
@@ -1,28 +1,37 @@
 # for all my simple RSS needs on the CLI
 import feedparser
 
-mangastream_rss_url = "http://mangastream.com/rss"
-mangafox_lookism_rss_url = "http://mangafox.me/rss/lookism.xml"
+MANGASTREAM_RSS_URL = "http://mangastream.com/rss"
+MANGAFOX_LOOKISM_RSS_URL = "http://mangafox.me/rss/lookism.xml"
 
-feed = feedparser.parse(mangastream_rss_url)
-entries = []
+def get_mangastream():
+    feed = feedparser.parse(MANGASTREAM_RSS_URL)
+    entries = []
 
-for entry in feed.entries:
-    title_and_date = [entry.title, entry.published]
-    entries.append(title_and_date)
+    for entry in feed.entries:
+        title_and_date = [entry.title, entry.published]
+        entries.append(title_and_date)
 
-# print out entries with the most recent output last
-entries.reverse()
-print("MANGASTREAM:\n-------------------------------")
-for entry in entries:
-    print(entry[0] + "\n" + entry[1] + "\n")
+    # print out entries with the most recent output last
+    entries.reverse()
+    print("MANGASTREAM:\n-------------------------------")
+    for entry in entries:
+        print(entry[0] + "\n" + entry[1] + "\n")
 
-# gimme the last two chapters
-feed = feedparser.parse(mangafox_lookism_rss_url)
-print("LOOKISM:\n-------------------------------")
-counter = 0
-for entry in feed.entries:
-    if counter > 1:
-        break
-    print(entry.title + "\n" + entry.published + "\n")
-    counter = counter + 1
+def get_lookism():
+    # gimme the last two chapters
+    feed = feedparser.parse(MANGAFOX_LOOKISM_RSS_URL)
+    print("LOOKISM:\n-------------------------------")
+    counter = 0
+    for entry in feed.entries:
+        if counter > 1:
+            break
+        print(entry.title + "\n" + entry.published + "\n")
+        counter = counter + 1
+
+def main():
+    get_mangastream()
+    get_lookism
+
+if __name__ == "__main__":
+    main()
